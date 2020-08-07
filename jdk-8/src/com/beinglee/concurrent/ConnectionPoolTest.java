a2d72d6caa42959c2193f61f7eb32bebbe69c5e2
zhanglu <yibeichou@foxmail.com>
2020-08-07 11:57:21 +0800

    Java并发编程-一个简单的数据库连接池

---
+++ jdk-8/src/com/beinglee/concurrent/ConnectionPoolTest.java
@@ -0,0 +1,76 @@
+package com.beinglee.concurrent;
+
+import java.sql.Connection;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * @author Luz
+ * @date 2020/8/7
+ */
+public class ConnectionPoolTest {
+
+    private static final ConnectionPool POOL = new ConnectionPool(10);
+    private static CountDownLatch start = new CountDownLatch(1);
+    private static CountDownLatch end;
+
+    public static void main(String[] args) throws InterruptedException {
+        int threadCount = 20;
+        end = new CountDownLatch(threadCount);
+        int count = 20;
+        AtomicInteger got = new AtomicInteger();
+        AtomicInteger notGot = new AtomicInteger();
+        for (int i = 0; i < threadCount; i++) {
+            Thread t = new Thread(new ConnectionRunner(count, got, notGot), "ConnectionRunnerThread");
+            t.start();
+        }
+        start.countDown();
+        end.await();
+        System.out.println("total invoke:" + (threadCount * count));
+        System.out.println("got connection:" + got);
+        System.out.println("not got connection:" + notGot);
+    }
+
+    static class ConnectionRunner implements Runnable {
+        private int count;
+        private AtomicInteger got;
+        private AtomicInteger notGot;
+
+        public ConnectionRunner(int count, AtomicInteger got, AtomicInteger notGot) {
+            this.count = count;
+            this.got = got;
+            this.notGot = notGot;
+        }
+
+        @Override
+        public void run() {
+            try {
+                start.await();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            while (count > 0) {
+                try {
+                    Connection connection = POOL.fetchConnection(1000);
+                    if (connection != null) {
+                        try {
+                            connection.createStatement();
+                            connection.commit();
+                        } finally {
+                            POOL.releaseConnection(connection);
+                            got.incrementAndGet();
+                        }
+                    } else {
+                        notGot.incrementAndGet();
+                    }
+                } catch (Exception e) {
+
+                } finally {
+                    count--;
+                }
+            }
+            end.countDown();
+        }
+    }
+
+}
